b132f4c88c4ad2aeb4226eb28f57f4551dd0471a
Samarkand N01 <technolifeno360chettel889563333@MacBook-Air.local>
2026-05-28 21:31:39 +0800

    add impact site verification meta tag

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="impact-site-verification"
+        content="69996f3c-e366-4eb7-9662-bcf219f580e5"
+      />
+    </>
+  );
+}
